7bcfbec84562f0a57b9f41d4dc8f7c29f0042c12
Haris Rizwan <harisrizwan@Hariss-MBP.local>
2017-12-11 20:22:50 -0500

    updated the contobox ajax request to run with flask server

--- ajax contobox/request.js
+++ ajax contobox/request.js
@@ -1,9 +1,9 @@
 function get_contobox_data(){
   $.ajax({
         type: 'POST',
-        url: "/Users/harisrizwan/SeleniumSpace/Contoboxtest/main_method.py",
+        url: "http://127.0.0.1:5000/test",
         data: {param: 18965}, //passing some input here
-        dataType: "text",
+        dataType: "JSON",
         success: function(response){
            output = response;
            alert(output);
