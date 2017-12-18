cf177572a51e5e53bed911b4496e201d1262357f
Haris Rizwan <harisrizwan@Hariss-MBP.local>
2017-12-18 16:21:31 -0500

    contobox ajax script updates

--- ajax contobox/request.js
+++ ajax contobox/request.js
@@ -1,11 +1,24 @@
 function get_contobox_data(){
+  var val = $("#add-id").val();
   $.ajax({
         type: 'POST',
         url: "http://127.0.0.1:5000/test",
-        data: {param: 18965}, //passing some input here
+        data: {'ad_id': val}, //passing some input here
         dataType: "JSON",
         success: function(response){
            output = response;
-           alert(output);
+           console.log(output);
         }})
 };
+
+$(document).ready(function(){
+    $(document).ajaxStart(function(){
+        $("#wait").css("display", "block");
+    });
+    $(document).ajaxComplete(function(){
+        $("#wait").css("display", "none");
+    });
+    $("button").click(function(){
+        $("#txt").load("demo_ajax_load.asp");
+    });
+});
