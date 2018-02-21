3562b677f8dba4334e6212d8c9ef2d7d9ac013d8
Haris Rizwan <harisrizwan@Hariss-MacBook-Pro.local>
2018-02-21 15:06:55 -0500

    updated test3.js still expandable methods need to be worked on

--- selenium-demo/test2.js
+++ selenium-demo/test2.js
@@ -7,11 +7,16 @@ var harCapturing = require('chrome-har-capturer');
 var fs = require('fs');
 var dataFrame = require('dataframe-js');
 
-var banner ="https://dbb1.contobox.com/v3/preview.php?id=20005"
-var previewExpandable ="https://dbb1.contobox.com/v3/preview.php?id=20005&tpl=preview_expanded"
+var banner ="https://dbb1.contobox.com/v3/preview.php?id=20055"
+var previewExpandable ="https://dbb1.contobox.com/v3/preview.php?id=20055&tpl=preview_expanded"
 var url_banner = [banner];
 var url_expandable = [previewExpandable];
 var UrlSize={};
+var sortable = [];
+
+
+
+
 
 harCapturing.run(url_banner).on('har', function (har) {
     var logs = har;
@@ -42,13 +47,60 @@ harCapturing.run(url_banner).on('har', function (har) {
     console.log("**********__________************");
     console.log("Number of http request = "+" "+httpLinks.length);
     console.log("**************-------------------**************");
-    console.log(transferedSize);
+    // console.log(transferedSize);
     console.log("Banner weight ="+" "+preExpandableWeight/1000+" "+"kb");
-    console.log(Object.keys(UrlSize).length);
 
     
    
 });
+
+
+
+harCapturing.run(url_expandable).on('har', function (har) {
+    var logs = har;
+    var entries = logs.log.entries;
+    var links=[];
+    var httpLinks = [];
+    var transferedSize=[];
+    var httpCheck_regex = /(http)\:\/\//gi;
+    for (var property in entries) {
+        var checkUrl= entries[property].request.url;
+        links.push(checkUrl);
+        bol_check = (checkUrl.match(httpCheck_regex));
+        // console.log(bol_check);
+        if(bol_check!==null){
+            httpLinks.push(checkUrl)
+        }
+        var checkSize = entries[property].response._transferSize;
+        transferedSize.push(checkSize);
+        UrlSize[checkUrl]=checkSize;
+        
+    }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    var preExpandableWeight = transferedSize.reduce(reducer);
+
+    for (var URLZ in UrlSize) {
+        sortable.push([URLZ, UrlSize[URLZ]]);
+    };
+
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    console.log("Total expandable request = "+" "+links.length);
+    console.log("**********__________************");
+    console.log("Number of http request = "+" "+httpLinks.length);
+    console.log("**************-------------------**************");
+    // console.log(transferedSize);
+    console.log("Expandable weight ="+" "+preExpandableWeight/1000+" "+"kb");
+    console.log("Total request of ContoBOX ="+" "+Object.keys(UrlSize).length);
+    // console.log(sortable);
+    
+
+           
+            
+});
+
 
 
 
---
+++ selenium-demo/test3.js
@@ -0,0 +1,125 @@
+var cmd = require('node-cmd');
+
+var cmd1 = 'sh runChromeHeadless.sh';
+cmd.run(cmd1);
+
+var harCapturing = require('chrome-har-capturer');
+var fs = require('fs');
+
+var co = require('co');
+
+
+var banner = "https://dbb1.contobox.com/v3/preview.php?id=20005"
+var previewExpandable = "https://dbb1.contobox.com/v3/preview.php?id=20005&tpl=preview_expanded"
+var urlz = [banner, previewExpandable];
+let harData;
+var bannerHar = [];
+var expHar = [];
+var preLinks = [];
+var preError = {};
+var prehttplinks = [];
+var preUrlSize = {};
+var expUrlSize= {};
+// var urlSize = {};
+var preWeight;
+var httpCheck_regex = /(http)\:\/\//gi;
+
+function getData(link) {
+
+    return new Promise(function (res, rej) {
+        harCapturing.run(link).on('har', function (har) {
+            var logs = har;
+            // console.log(logs); 
+            var filteredLogs = logs.log.entries;
+            // console.log(filteredLogs);
+            res(filteredLogs);
+        });
+    });
+    console.log("it works first");
+};
+
+function sortHar() {
+    for (var values in harData) {
+        if (harData[values].pageref == 'page_1')
+            bannerHar.push(harData[values]);
+        else {
+            expHar.push(harData[values]);
+        };
+
+    };
+
+    console.log("data sorted");
+
+};
+
+function bannerHarSorting() {
+    var transferedSize = [];
+    for (values in bannerHar) {
+        var x = bannerHar[values].request.url;
+        preLinks.push(x);
+
+        // checking the url for HTTP 
+
+        bol_check = (x.match(httpCheck_regex));
+        if (bol_check !== null) {
+            prehttplinks.push(x);
+        };
+
+        // getting the transfered size of the responses
+
+        var checkSize = bannerHar[values].response._transferSize;
+        transferedSize.push(checkSize);
+        preUrlSize[x] = checkSize;
+
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        preWeight = transferedSize.reduce(reducer);
+
+        // checking response for status 403 and 404
+
+        if(bannerHar[values].response.status==403 || bannerHar[values].response.status==404){
+
+            preError[x]=bannerHar[values].response.status;
+        };
+
+
+
+
+    }
+
+    console.log("banner results done");
+
+};
+
+
+function testDataWithCo() {
+    co(function* () {
+        harData = yield getData(urlz);
+        sortHar();
+        bannerHarSorting();
+        console.log(preUrlSize);
+        console.log(preError);
+        // console.log(expHar);
+          
+             
+    });
+};
+
+// getData(urlz).then(function (r) {
+//     harData = r;
+    
+// });
+// var url_banner = [banner];
+
+// harCapturing.run(url_banner).on('har', function (har) {
+//     var logs = har;
+//     // console.log(logs);
+//     var filteredLogs = logs.log.entries;
+//     console.log(filteredLogs);
+// });
+testDataWithCo();
+
+
+
+
+
+
